42ef7433dedd61d630f2664ad45accd633552eb8
Mayssatbr <mayssatbr3@gmail.com>
2020-10-16 22:02:08 -0400

    Add files via upload

---
+++ app/src/main/res/layout/Golf.java
@@ -0,0 +1,14 @@
+package com.example.emty_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Golf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_golf);
+    }
+}
